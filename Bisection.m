03f46b43d97740eac51942c17837c38d96d02e21
autul2017831021 <43562788+autul2017831021@users.noreply.github.com>
2019-10-30 04:50:40 +0600

    Update Bisection.m

--- Bisection.m
+++ Bisection.m
@@ -1,27 +1,26 @@
-function value = Bisection(f, a, b)
-  A(1) = a;
-  B(1) = b;
-  c(1) = (a+b)/2;
-  fa(1) = f(a);
-  fb(1) = f(b);
-  fc(1) = f(c(1));
-  
-  for i = 2:20
-
-    if fa(i-1)*fc(i-1) < 0 
-      B(i) = c(i-1);
-      A(i) = A(i-1);
-    else
-      A(i) = c(i-1);
-      B(i) = B(i-1);
-    endif
-    
-    fa(i) = f(A(i));
-    fb(i) = f(B(i));
-    c(i) = (A(i)+B(i))/2;
-    fc(i) = f(c(i));
-    
-  endfor
-  value = [A' B' fa' fb' c' fc'];
-  
-endfunction
+f=@(x) x^2-4
+a=input('enter a')
+b=input('enter b')
+if f(a)*f(b)>0
+  fprintf('no root')
+  return
+endif
+if f(a)==0
+  fprintf('a is one of the root')
+  return
+elseif f(b)==0
+  fprintf('b is one of the root')
+  return
+  endif
+  for i=1:100
+  c=(a+b)/2;
+  if f(a)*f(c)<0
+    b=c;
+  else
+    a=c;
+  endif
+  if abs(f(a))<1e-6
+    break;
+  endif
+endfor
+fprintf('%f %d',a,i)
