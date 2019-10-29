da0ff2cec65d6182da73cb719d25fb8d9ec34fb5
autul2017831021 <43562788+autul2017831021@users.noreply.github.com>
2019-10-30 04:51:27 +0600

    Update Rungekutta.m

--- Rungekutta.m
+++ Rungekutta.m
@@ -1,11 +1,11 @@
-function retval = Rungekutta(f, xrange, y0, h)
-  x = xrange(1):h:xrange(2);
-  y(1) = y0;
-  for i = 2:length(x)
-    k1 = h*f(x(i-1), y(i -1));
-    k2 = h*f(x(i-1)+h, y(i-1)+k1);
-    y(i) = y(i-1)+0.5*(k1+k2);
-  endfor
-  
-  retval = [x' y'];
-endfunction
+f = @(k,l) ((k^2)+(l^2))/10 ;
+x = input('x0 : ');
+y = input('y0 : ');
+h = input('h : ');
+for i = 0:10
+  printf("%d %d\n",x,y);
+  k1 = h*f(x,y);
+  k2 = h*f(x+h,y+k1);
+  y = y + (k1+k2)/2;
+  x = x +h ;
+endfor
